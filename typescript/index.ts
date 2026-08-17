301e0f4806d3e66f2808f38cf261fdffdc08ad20
akshat-dev018 <akshat5august2007@gmail.com>
2026-08-17 22:59:44 +0530

    typescript remaning topics

--- typescript/index.ts
+++ typescript/index.ts
@@ -22,3 +22,36 @@ let arr: any[] = [1,2,3,4,5,6,"try",true];
 // unkown=> kuch bhi do pr update krte wkt dikkat hogi..kyuki unkown data type hai toh merko nhi pta kisse update krna
 // hum unkown api ke time use krte kyuki tb hume pata nhi hota ki number ayega , string ayega kya ayega 
 // never => kuch bhi nhi aana chaiye
+
+
+let arr1:string[] = ["akshat","tiwari"];
+let arr2 : boolean[] = [true,false,true,false];
+
+// TUPLES
+let arr3:[number,number,string,boolean] = [23,45,"hello",true];
+
+// HOW TO WRITE ARRAY OF OBJECTS IN TUPLES
+let data : [{name:string},{name:number},{name:boolean}] = [
+    {name:"pol"},{name:12},{name:true}
+]
+
+// ENUMS => Option
+
+enum Role{
+    ADMIN,
+    SUP_ADMIN,
+    USER,
+}
+
+let role : Role = Role.ADMIN;
+// in 3 options se hi kuch select kr skte ho 
+
+// UNION => JB EK SE JYDA KI NEED HO VARIABLE KO BANANE KE LIYE TOH TB HUM UNION USE KRTE HAI
+let yolo: string|number|boolean|bigint="rahul";
+yolo = 45;
+yolo =90n;
+yolo: false;
+
+// LITERALS => JB VALUES MEIN AAP OPTION DE RHE HOTE HO
+type Status = "pending"|"success"|"error";
+let status:Status="success";
